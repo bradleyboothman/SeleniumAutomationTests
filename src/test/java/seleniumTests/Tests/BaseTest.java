d54378f45516fcdd8983d6126ab594c8aaeefb64
Bradley Boothman <32442515+bbowesbo@users.noreply.github.com>
2021-06-16 23:56:06 +0100

    Browser zoom added to base test

--- src/test/java/seleniumTests/Tests/BaseTest.java
+++ src/test/java/seleniumTests/Tests/BaseTest.java
@@ -5,7 +5,10 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 // This class is here to hold all of the driver lifecycle management code
@@ -17,10 +20,17 @@ public class BaseTest {
     @BeforeClass
     public static void setUp(){
     	
+    	// Locate chrome driver
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/windows/chromedriver.exe");
 		driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        // Maximize browser window
         driver.manage().window().maximize();
+        
+        // Set browser zoom to 100%
+        WebElement html = driver.findElement(By.tagName("html"));
+        html.sendKeys(Keys.chord(Keys.CONTROL, "0")); 
 
     }
 
